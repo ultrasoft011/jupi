50975117f5f6fc7ed25ee22f55c6a268166d0663
ultrasoft011 <ultrasoft.011@gmail.com>
2021-12-01 12:46:33 -0500

    Navbar import and export

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Jupi E-commerce</p>
