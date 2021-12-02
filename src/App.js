bb79e016ef7e60c3fdb7d9f1ac3f50b695fa98f2
ultrasoft011 <ultrasoft.011@gmail.com>
2021-12-01 22:56:51 -0500

    Navbar logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/NavBar";
+import logo from "./images/jupiter.png";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Jupi E-commerce</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React with CoderHouse
-        </a>
       </header>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,26 @@
 import React from "react";
+import logo from "../images/jupiter.png";
 
 function Navbar() {
   return (
-    <div>
-      <nav>
-        <div class="nav-wrapper">
-          <a class="brand-logo center">Logo</a>
-          <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li>
-              <a href="#">Sass</a>
-            </li>
-            <li>
-            <a href="#">Sass</a>
-            </li>
-            <li>
-            <a href="#">Sass</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <div className="navbar">
+      <ul class="#d7ccc8 brown lighten-4">
+        <li>
+        <img src={logo} width="150px" />
+        </li>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#news">News</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Us</a>
+        </li>
+        <li>
+          <a href="#about">About Jupi</a>
+        </li>
+      </ul>
     </div>
   );
 }
