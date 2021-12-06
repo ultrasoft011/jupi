336be0205089636eda881176be2df347f022d6e5
ultrasoft011 <ultrasoft.011@gmail.com>
2021-12-06 14:06:54 -0500

    React-bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,55 +2,82 @@ import React from "react";
 import logo from "../images/jupiter.png";
 import CartWidget from "./CartWidget";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div className="navbar">
-      <ul class="#f3e5f5 purple lighten-5">
-        <div class="grid-container">
-          <li>
-            {" "}
-            <img class="jupiter-logo" src={logo} width="85px" />
-          </li>
-          <li>
-            <i class="material-icons">home</i>
-            <a href="#contact">Home</a>
-          </li>
-          <li>
-            <i class="material-icons">store_mall_directory</i>
-            <a href="#about">News</a>
-          </li>
-          <li class="search-bar"></li>
-
-          <li>
-            <i class="material-icons">people_outline</i>
-            <a href="#contact">Contact Us</a>
-          </li>
-          <li>
-            <i class="material-icons">web</i>
-            <a href="#about">About Jupi</a>
-          </li>
-          <div class="grid-container">
-            <li class="search-bar">
-              <nav>
-                <div class="nav-wrapper #424242 grey darken-3">
-                  <form>
-                    <div class="input-field ">
-                      <input id="search" type="search" required />
-                      <label class="label-icon" for="search">
-                        <i class="material-icons ">search</i>
-                      </label>
-                      <i class="material-icons ">close</i>
-                    </div>
-                  </form>
-                </div>
-              </nav>
-            </li>
-          </div>
-          <CartWidget />
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col-1">Column</div>
+          <div class="col-2">Column</div>
+          <div class="col-9">Column</div>
         </div>
-      </ul>
-    </div>
+      </div>
+    </>
   );
-}
+};
+
+//     <div className="navbar">
+
+// <div class="container">
+//   <div class="row">
+//     <div class="col">
+//       Column
+//     </div>
+//     <div class="col">
+//       Column
+//     </div>
+//     <div class="col">
+//       Column
+//     </div>
+//   </div>
+// </div>
+//       <ul class="#f3e5f5 purple lighten-5">
+//         <div class="grid-container">
+//           <li>
+//             {" "}
+//             <img class="jupiter-logo" src={logo} width="85px" />
+
+//           </li>
+//           <li>
+//             <i class="material-icons">home</i>
+//             <a href="#contact">Home</a>
+//           </li>
+//           <li>
+//             <i class="material-icons">store_mall_directory</i>
+//             <a href="#about">News</a>
+//           </li>
+//           <li class="search-bar"></li>
+
+//           <li>
+//             <i class="material-icons">people_outline</i>
+//             <a href="#contact">Contact Us</a>
+//           </li>
+//           <li>
+//             <i class="material-icons">web</i>
+//             <a href="#about">About Jupi</a>
+//           </li>
+//           <div class="grid-container">
+//             <li class="search-bar">
+//               <nav>
+//                 <div class="nav-wrapper #424242 grey darken-3">
+//                   <form>
+//                     <div class="input-field ">
+//                       <input id="search" type="search" required />
+//                       <label class="label-icon" for="search">
+//                         <i class="material-icons ">search</i>
+//                       </label>
+//                       <i class="material-icons ">close</i>
+//                     </div>
+//                   </form>
+//                 </div>
+//               </nav>
+//             </li>
+//           </div>
+//           <CartWidget />
+//         </div>
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default Navbar;
---
+++ src/components/test.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Testing = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testing;
